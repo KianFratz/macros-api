d509038b8054c08c3ceb2c0e43b192bfbb462af8
KianFratz <fratzkian@gmail.com>
2025-12-24 16:17:22 +0800

    User controller and routes done

--- src/api/v1/routes/userRoutes.ts
+++ src/api/v1/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/userController.js";
 
 const userRoutes = express.Router();
 
-userRoutes.post("/user", createUser);
-userRoutes.get("/user", getAllUsers);
-userRoutes.get("/user/:id", getUserById);
-userRoutes.delete("/user/:id", deleteUser);
-userRoutes.put("/user/:id", updateUser);
+userRoutes.post("/users", createUser);
+userRoutes.get("/users", getAllUsers);
+userRoutes.get("/users/:id", getUserById);
+userRoutes.delete("/users/:id", deleteUser);
+userRoutes.put("/users/:id", updateUser);
 
 export default userRoutes;
