e1f12651e8845138a808b9d1b80b6dcb46e1c84f
KianFratz <fratzkian@gmail.com>
2025-12-27 19:51:37 +0800

    createCategoryTable.ts

---
+++ src/api/v1/data/createCategoryTable.ts
@@ -0,0 +1,21 @@
+import pool from "../../../config/db.js";
+
+const createCategoryTable = async () => {
+    const queryText = `
+        CREATE TABLE IF NOT EXISTS categories (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) UNIQUE NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW()
+    )
+    `;
+
+    try {
+        await pool.query(queryText);
+        console.log("Category table created if not exists")
+
+    } catch (error) {
+        console.log("Error in creating category table: ", error)
+    }
+};
+
+export default createCategoryTable;
