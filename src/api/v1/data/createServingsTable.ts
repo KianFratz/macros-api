4a4ca663884c0c63ddcd731ded74d7e2de84f367
KianFratz <fratzkian@gmail.com>
2026-01-06 19:31:35 +0800

    Added createServingsTable schema

---
+++ src/api/v1/data/createServingsTable.ts
@@ -0,0 +1,24 @@
+import pool from "../../../config/db.js";
+
+const createServingsTable = async () => {
+    const queryText = `
+        CREATE TABLE IF NOT EXISTS servings {
+            serving_id SERIAL PRIMARY KEY,
+            food_id INTEGER NOT NULL,
+            serving_name VARCHAR(100) NOT NULL,
+            grams FLOAT NOT NULL,
+            created_at TIMESTAMP DEFAULT NOW(),
+            FOREIGN KEY (food_id) REFERENCES food(food_id)
+        }
+    `;
+
+    try {
+        await pool.query(queryText);
+        console.log("Servings table created if not exists")
+
+    } catch (error) {
+        console.log("Error in creating servings table: ", error)
+    }
+};
+
+export default createServingsTable;
--- src/api/v1/data/data.sql
+++ src/api/v1/data/data.sql
@@ -34,3 +34,12 @@ CREATE TABLE IF NOT EXISTS nutrition (
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY (food_id) REFERENCES food(food_id)
 )
+
+CREATE TABLE IF NOT EXISTS servings {
+    serving_id SERIAL PRIMARY KEY,
+    food_id INTEGER NOT NULL,
+    serving_name VARCHAR(100) NOT NULL,
+    grams FLOAT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (food_id) REFERENCES food(food_id)
+}
