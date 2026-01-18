d6df66fea161163377ef321d9d9d807b271eb11b
KianFratz <fratzkian@gmail.com>
2026-01-18 18:15:00 +0800

    Integrated auth in nutrition route

--- src/api/v1/routes/nutritionRoute.ts
+++ src/api/v1/routes/nutritionRoute.ts
@@ -1,14 +1,18 @@
 import  express  from "express";
 import { createNutrition, deleteNutrition, getAllNutrition, getNutritionById, updateNutrition } from "../controllers/nutritionController.js";
-import { get } from "node:http";
-import type { deleteNutritionService } from "../models/nutritionModel.js";
+import { authenticate } from "../middlewares/auth.js";
+import { authorize } from "../middlewares/authorize.js";
 
 const nutritionRoutes = express.Router();
 
-nutritionRoutes.post("/nutrition", createNutrition);
+// Public routes
 nutritionRoutes.get("/nutrition", getAllNutrition);
 nutritionRoutes.get("/nutrition/:id", getNutritionById);
-nutritionRoutes.delete("/nutrition/:id", deleteNutrition);
-nutritionRoutes.put("/nutrition/:id", updateNutrition);
+
+
+// Protected routes - only authenticated users with specific roles
+nutritionRoutes.post("/nutrition", authenticate, authorize("admin", "manager"), createNutrition);
+nutritionRoutes.delete("/nutrition/:id", authenticate, authorize("admin", "manager"), deleteNutrition);
+nutritionRoutes.put("/nutrition/:id", authenticate, authorize("admin", "manager"), updateNutrition);
 
 export default nutritionRoutes;
