2ddeb658e987735505a95ff89f26a7aa8688c268
KianFratz <fratzkian@gmail.com>
2026-01-24 12:27:34 +0800

    Applied Cookie-based JWT to protect against cross site scripting (XSS) and cross site request forgery (CSRF) attacks and added logout endpoint

--- src/api/v1/controllers/authController.ts
+++ src/api/v1/controllers/authController.ts
@@ -7,8 +7,6 @@ export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
     
-    
-
     if (!email || !password) {
         return res.status(400).json({ message: "Email and password are required" });
     }
@@ -35,6 +33,13 @@ export const login = async (req: Request, res: Response) => {
       { expiresIn: "1h" }
     );
 
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: (1000 * 60 * 60 * 24) * 7
+    })
+
     res.status(200).json({
       token,
       user: {
@@ -48,3 +53,15 @@ export const login = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const logout = (req: Request, res: Response) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  })
+
+  res.status(200).json({
+    status: "success",
+    message: "Logged out successfully",
+  })
+}
--- src/api/v1/routes/authRoute.ts
+++ src/api/v1/routes/authRoute.ts
@@ -1,10 +1,11 @@
 import  express  from "express";
-import { login } from "../controllers/authController.js";
+import { login, logout } from "../controllers/authController.js";
 
 const authRoutes = express.Router();
 
 // Public routes
 authRoutes.post("/login",  login);
+authRoutes.post("/logout", logout)
 
 // Protected routes
 
