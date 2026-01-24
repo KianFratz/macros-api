c540bb4e49fdbdeb0dcd9521f8fae79d77186c4d
KianFratz <fratzkian@gmail.com>
2026-01-24 11:32:47 +0800

    Added auth in user route

--- src/api/v1/routes/userRoutes.ts
+++ src/api/v1/routes/userRoutes.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/userController.js";
+import { authorize } from "../middlewares/authorize.js";
+import { authenticate } from "../middlewares/auth.js";
 
 const userRoutes = express.Router();
 
+// Public route
 userRoutes.post("/users", createUser);
-userRoutes.get("/users", getAllUsers);
-userRoutes.get("/users/:id", getUserById);
-userRoutes.delete("/users/:id", deleteUser);
-userRoutes.put("/users/:id", updateUser);
+
+// Private route
+userRoutes.get("/users",  authenticate, authorize("manager", "admin"), getAllUsers);
+userRoutes.get("/users/:id", authenticate, authorize("manager", "admin"), getUserById);
+userRoutes.delete("/users/:id", authenticate, authorize("manager", "admin"), deleteUser);
+userRoutes.put("/users/:id", authenticate, authorize("manager", "admin"), updateUser);
 
 export default userRoutes;
